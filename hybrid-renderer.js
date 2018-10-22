ca949c6458b7e59f7b0435117062cac6f4a13c73
Jacob Hochendoner <jacob@jrhtec.com>
2018-10-22 14:26:37 -0400

    initial commit

---
+++ hybrid-renderer.js
@@ -0,0 +1,20 @@
+const $webview = document.querySelector('webview');
+const $loader = document.querySelector('.loader');
+let isInitialLoad = true;
+
+$webview.addEventListener('did-start-loading', () => {
+  // we use client side rendering in the web app, so the loader is only needed on the first page load
+  if(isInitialLoad) {
+    $webview.classList.add('hide');
+    $loader.classList.remove('loader-hide');
+    isInitialLoad = false;
+  }
+});
+
+$webview.addEventListener('dom-ready', () => {
+  $webview.classList.remove('hide');
+  // have to delay in order for the webview show/resize to settle
+  setTimeout(() => {
+    $loader.classList.add('loader-hide');
+  }, 100);
+});
